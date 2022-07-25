731a20a083c93f267e3a4039b0b52dea2324a014
Karol Zyśk <trener.karolzysk@gmail.com>
2022-07-25 12:19:32 +0200

    Intro layout change, image animation added

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Intro.module.css";
+import { motion, Variants } from "framer-motion";
 import Image from "next/image";
 import introImage1 from "../../public/images/intro/introImage1.jpg";
 import introImage2 from "../../public/images/intro/introImage2.jpg";
@@ -8,6 +9,17 @@ import introImage4 from "../../public/images/intro/introImage4.jpg";
 import Circle from "../Circle/Circle";
 
 const images = [introImage1, introImage2, introImage3, introImage4];
+
+const imageAnimate = {
+  offscreen: { x: 0, y: 200, opacity: 0 },
+  onscreen: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+
+    transition: { type: "spring",  duration: 1.2 },
+  },
+};
 
 const Intro = () => {
   return (
@@ -18,28 +30,58 @@ const Intro = () => {
           Witajcie<span className={styles.brand}> Tusinku</span>
         </h1>
         <p className={styles.description}>
-          <b style={{fontSize: "1.8rem", color: "darkgreen"}}>Tusinek</b> jest położony przy drodze na Mazury w
-          miejscowości Rozogi. Przy wjeździe znajduje się restauracja, w której
-          serwowane są wyśmienite <b>regionalne potrawy</b>. W głębi{" "}
-          <i>Alei Lipowej</i>, za restauracją znajduje się część hotelowa oraz{" "}
-          <b>gospodarstwo</b>.
+          <b style={{ fontSize: "1.8rem", color: "darkgreen" }}>Tusinek</b> jest
+          położony przy drodze na Mazury w miejscowości Rozogi. Przy wjeździe
+          znajduje się restauracja, w której serwowane są wyśmienite{" "}
+          <b>regionalne potrawy</b>. W głębi <i>Alei Lipowej</i>, za restauracją
+          znajduje się część hotelowa oraz <b>gospodarstwo</b>.
         </p>
         <button className={styles.button}>Sprawdź Ofertę</button>
       </div>
-      <div className={styles.imagesCard}>
-        {images.map((img, index) => {
-          return (
-            <div key={index} className={styles.imageWrapper}>
-              <Image
-                alt="Tusinek"
-                objectFit="fill"
-                src={img}
-                className={styles.image}
-              />
-            </div>
-          );
-        })}
-      </div>
+      <motion.div
+        className={styles.images_card}
+        transition={{ staggerChildren: 0.4 }}
+        initial={"offscreen"}
+        whileInView={"onscreen"}
+        viewport={{ once: false, amount: 0.2 }}
+      >
+        <motion.div
+          variants={imageAnimate}
+          className={styles.image_wrapper}
+          style={{ left: `5%`, top: ` 5%` }}
+        >
+          <Image
+            alt="Tusinek"
+            objectFit="fill"
+            src={introImage1}
+            className={styles.image}
+          />
+        </motion.div>
+        <motion.div
+          variants={imageAnimate}
+          className={styles.image_wrapper}
+          style={{ left: `45%`, top: `5% ` }}
+        >
+          <Image
+            alt="Tusinek"
+            objectFit="fill"
+            src={introImage2}
+            className={styles.image}
+          />
+        </motion.div>
+        <motion.div
+          variants={imageAnimate}
+          className={styles.image_wrapper}
+          style={{ left: `25%`, top: ` 45%` }}
+        >
+          <Image
+            alt="Tusinek"
+            objectFit="fill"
+            src={introImage3}
+            className={styles.image}
+          />
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
